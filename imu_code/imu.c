55acbd373045b4cd3683d6c20f72841a7f9923ff
moshah122 <moshah@seas.upenn.edu>
2025-04-21 21:18:45 -0400

    Update imu.c

--- imu_code/imu.c
+++ imu_code/imu.c
@@ -27,3 +27,31 @@ void ICM20948_readGyro(int16_t* gx, int16_t* gy, int16_t* gz) {
     *gy = (int16_t)((data[2] << 8) | data[3]);
     *gz = (int16_t)((data[4] << 8) | data[5]);
 }
+
+void dfPlayerPlay(uint16_t track) {
+    int sendBuffer[10] = {0x7E, 0xFF, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xEF};
+    sendBuffer[3] = 0x03;
+    sendBuffer[5] = track >> 8;
+    sendBuffer[6] = track & 0xFF;
+    uint16_t checksum = 0xFFFF - (0xFF + 0x06 + 0x03 + 0x00 + sendBuffer[5] + sendBuffer[6]) + 1;
+    sendBuffer[7] = (checksum >> 8);
+    sendBuffer[8] = (checksum & 0xFF);
+    for (int i = 0; i < 10; i++) { //sends over USART1
+        while (!(UCSR1A & (1 << UDRE1)));
+        UDR1 = sendBuffer[i];
+    }
+}
+
+void dfPlayerVolume(uint16_t volume) {
+    int sendBuffer[10] = {0x7E, 0xFF, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xEF};
+    sendBuffer[3] = 0x06;
+    sendBuffer[5] = volume >> 8;
+    sendBuffer[6] = volume & 0xFF;
+    uint16_t checksum = 0xFFFF - (0xFF + 0x06 + 0x06 + 0x00 + sendBuffer[5] + sendBuffer[6]) + 1;
+    sendBuffer[7] = (checksum >> 8);
+    sendBuffer[8] = (checksum & 0xFF);
+    for (int i = 0; i < 10; i++) { //sends over USART1
+        while (!(UCSR1A & (1 << UDRE1)));
+        UDR1 = sendBuffer[i];
+    }
+}
